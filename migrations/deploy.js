e3b3123d8e6c30138e913e4fd0623014365c4fb8
noobmdev <noobmdev@gmail.com>
2022-03-22 18:02:00 +0700

    deploy all contract before all test

--- migrations/deploy.js
+++ migrations/deploy.js
@@ -1,8 +1,21 @@
 const Box = artifacts.require("Box");
 const PermissionsUpgradable = artifacts.require("PermissionsUpgradable")
+const AccountManager = artifacts.require("AccountManager")
+const NodeManager = artifacts.require("NodeManager")
+const OrgManager = artifacts.require("OrgManager")
+const PermissionsImplementation = artifacts.require("PermissionsImplementation")
+const PermissionsInterface = artifacts.require("PermissionsInterface")
+const RoleManager = artifacts.require("RoleManager")
+const VoterManager = artifacts.require("VoterManager")
 
 module.exports = function (deployer) {
   deployer.deploy(Box);
   deployer.deploy(PermissionsUpgradable);
-  
+  deployer.deploy(AccountManager);
+  deployer.deploy(NodeManager);
+  deployer.deploy(OrgManager);
+  deployer.deploy(PermissionsImplementation);
+  deployer.deploy(PermissionsInterface);
+  deployer.deploy(RoleManager); 
+  deployer.deploy(VoterManager);
 };
--- test/test.js
+++ test/test.js
@@ -1,23 +1,56 @@
 // test/Box.test.js
 // Load dependencies
-const { expect } = require('chai');
+const { expect } = require("chai");
 
 // Load compiled artifacts
-const PermissionsUpgradable = artifacts.require('PermissionsUpgradable');
+const PermissionsUpgradable = artifacts.require("PermissionsUpgradable");
+const AccountManager = artifacts.require("AccountManager");
+const NodeManager = artifacts.require("NodeManager");
+const OrgManager = artifacts.require("OrgManager");
+const PermissionsImplementation = artifacts.require(
+  "PermissionsImplementation"
+);
+const PermissionsInterface = artifacts.require("PermissionsInterface");
+const RoleManager = artifacts.require("RoleManager");
+const VoterManager = artifacts.require("VoterManager");
 
 // Start test block
-contract('PermissionsUpgradable', function ([owner, account1, account2]) {
-  beforeEach(async function () {
+contract("PermissionsUpgradable", function ([owner, account1, account2]) {
+  before(async function () {
     // Deploy a new Box contract for each test
     this.permissionsUpgradable = await PermissionsUpgradable.new(owner);
+    const results = await Promise.all([
+      AccountManager.new(this.permissionsUpgradable.address),
+      NodeManager.new(this.permissionsUpgradable.address),
+      OrgManager.new(this.permissionsUpgradable.address),
+      RoleManager.new(this.permissionsUpgradable.address),
+      VoterManager.new(this.permissionsUpgradable.address),
+      PermissionsInterface.new(this.permissionsUpgradable.address),
+    ]);
+    this.accountManager = results[0];
+    this.nodeManager = results[1];
+    this.orgManager = results[2];
+    this.roleManager = results[3];
+    this.voterManager = results[4];
+    this.permissionsInterface = results[5];
+
+    this.permissionsImplementation = await PermissionsImplementation.new(
+      this.permissionsUpgradable.address,
+      this.orgManager.address,
+      this.roleManager.address,
+      this.accountManager.address,
+      this.voterManager.address,
+      this.nodeManager.address
+    );
+    // this.accountManager = await AccountManager.new(this.permissionsUpgradable.address);
+    // this.accountManager = await AccountManager.new(this.permissionsUpgradable.address);
   });
 
   // Test case
-  it('check getGuardian', async function () {
-
+  it("getGuardian", async function () {
+    console.log(this.permissionsImplementation);
     // Test if the returned value is the same one
     // Note that we need to use strings to compare the 256 bit integers
-    expect((await this.permissionsUpgradable.getGuardian())).to.equal(owner);
-    console.log("2")
+    expect(await this.permissionsUpgradable.getGuardian()).to.equal(owner);
   });
 });
