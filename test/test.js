b5aff539c1a0c220e086e2a8c8c3abbaea195100
noobmdev <noobmdev@gmail.com>
2022-03-22 17:21:26 +0700

    init test case

--- test/test.js
+++ test/test.js
@@ -18,5 +18,6 @@ contract('PermissionsUpgradable', function ([owner, account1, account2]) {
     // Test if the returned value is the same one
     // Note that we need to use strings to compare the 256 bit integers
     expect((await this.permissionsUpgradable.getGuardian())).to.equal(owner);
+    console.log("2")
   });
 });
